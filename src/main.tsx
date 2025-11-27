733f0c1ab429c35cc889bafb32a24f19cb116788
Anuj Parakh <anuj@anujinfotech.com>
2025-11-27 10:53:02 -0600

    Added ts-ignore for css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { render } from 'preact'
 import { App } from '@/app'
 import { SettingsProvider } from '@/components/SettingsProvider'
+// @ts-ignore: allow side-effect import of CSS without type declarations
 import '@/index.css'
 
 const root = document.getElementById('app')
