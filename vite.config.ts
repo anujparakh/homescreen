d0705c5f8cc35e32fb6c29b5776c62799284d1c3
Anuj Parakh <anuj@anujinfotech.com>
2025-11-28 00:10:15 -0600

    Fixing pipeline, removing gh deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import path from 'path'
 
-const isPages = process.env.GITHUB_PAGES === 'true'
-
 export default defineConfig({
   plugins: [preact()],
   server: {
     port: 3003,
   },
-  base: isPages ? '/homescreen' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
